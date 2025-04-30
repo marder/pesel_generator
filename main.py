548677717830522903ca27522dc52f183dd9d703
marder <marcin.derucki@gmail.com>
2025-04-30 19:29:14 +0200

    30.04.2025

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from pesel import Pesel
 
 def main():
 
-    person = Pesel('1999', '990201', 'M')
+    person = Pesel('1988', '880505', 'M')
     print(person.generate_number())
     print(person.is_valid('99020151665', 'M'))  # False
     print(person.is_valid('99020151665', 'F'))  # True
-    print(person.is_valid('99020139551', 'M'))  # True
+    print(person.is_valid('88050511453', 'M'))  # True
     print(person.is_valid('99020139551', 'F'))  # False
 
 
